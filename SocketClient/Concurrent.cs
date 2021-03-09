c7a0abbff344110da106315a8e2b37c957fe1789
Thomas Visser <t.visser@salvux.com>
2021-03-09 20:30:31 +0100

    Initial

---
+++ SocketClient/Concurrent.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks; // 👈
+using Sequential;
+
+namespace Concurrent
+{
+    public class ConcurrentClient : SimpleClient
+    {
+        public Thread workerThread;
+
+        public ConcurrentClient(int id, Setting settings) : base(id, settings)
+        {
+            // todo [Assignment]: implement required code
+        }
+        public void run()
+        {
+            // todo [Assignment]: implement required code
+        }
+    }
+    public class ConcurrentClientsSimulator : SequentialClientsSimulator
+    {
+        private ConcurrentClient[] clients;
+
+        public ConcurrentClientsSimulator() : base()
+        {
+            Console.Out.WriteLine("\n[ClientSimulator] Concurrent simulator is going to start with {0}", settings.experimentNumberOfClients);
+            clients = new ConcurrentClient[settings.experimentNumberOfClients];
+            // 👇
+            configure();
+            for (int i = 0; i < settings.experimentNumberOfClients; i++)
+            {
+                clients[i] = new ConcurrentClient(i + 1, settings); // id>0 means this is not a terminating client
+            }
+            // 👆
+        }
+
+        public void ConcurrentSimulation()
+        {
+            try
+            {
+                // todo [Assignment]: implement required code
+                // 👇
+                var options = new ParallelOptions();
+                Parallel.For(0, settings.experimentNumberOfClients, options, (i) =>
+                {
+                    clients[i].prepareClient();
+                    clients[i].communicate();
+                });
+                // 👆
+            }
+            catch (Exception e)
+            { Console.Out.WriteLine("[Concurrent Simulator] {0}", e.Message); }
+
+            // 👇
+            Console.Out.WriteLine("\n[Concurrentimulator] All clients finished with their communications ... ");
+            Thread.Sleep(settings.delayForTermination);
+            SimpleClient endClient = new SimpleClient(-1, settings);
+            endClient.prepareClient();
+            endClient.communicate();
+            // 👆
+        }
+    }
+}
---
+++ SocketServer/Concurrent.cs
@@ -0,0 +1,100 @@
+﻿using Sequential;
+using System;
+//todo [Assignment]: add required namespaces
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace Concurrent
+{
+    public class ConcurrentServer : SequentialServer
+    {
+        // todo [Assignment]: implement required attributes specific for concurrent server
+        TcpListener server = null;
+
+        public ConcurrentServer(Setting settings) : base(settings)
+        {
+            // todo [Assignment]: implement required code
+            Console.WriteLine("start1");
+            this.settings = settings;
+            this.ipAddress = IPAddress.Parse(settings.serverIPAddress);
+
+        }
+        public override void prepareServer()
+        {
+            Console.WriteLine("start2");
+            Console.WriteLine(this.ipAddress.ToString() + settings.serverPortNumber);
+
+            localEndPoint = new IPEndPoint(this.ipAddress, settings.serverPortNumber);
+            listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            listener.Bind(localEndPoint);
+            listener.Listen(settings.serverListeningQueue);
+
+            // todo 4: analyse details of this listening loop. Everything in the server starts here.
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Waiting for incoming connections ... ");
+                    Socket connection = listener.Accept();
+                    this.numOfClients++;
+                    Console.WriteLine("Connected!");
+
+                    Thread t = new Thread(() => this.handleConcurrentClient(connection));
+                    t.Start();
+                }
+                catch (SocketException e)
+                {
+                    Console.WriteLine("SocketException: {0}", e);
+                    server.Stop();
+                }
+            }
+
+            // todo [Assignment]: implement required code
+        }
+
+        public void handleConcurrentClient(Socket con)
+        {
+            string data = "", reply = "";
+            byte[] bytes = new Byte[bufferSize];
+
+            this.sendMessage(con, Message.ready);
+            int numByte = con.Receive(bytes);
+            data = Encoding.UTF8.GetString(bytes, 0, numByte);
+            reply = processMessage(data);
+            this.sendMessage(con, reply);
+        }
+
+        public override string processMessage(String msg)
+        {
+            //todo 6: check how received messages are processed and handled. 
+            Thread.Sleep(settings.serverProcessingTime);
+            string replyMsg = Message.confirmed;
+
+            try
+            {
+                switch (msg)
+                {
+                    case Message.terminate:
+                        //todo 7: when this case is executed?
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                        Console.WriteLine("[Server] received from the client -> {0} ", msg);
+                        Console.ResetColor();
+                        Console.WriteLine("[Server] END : number of clients communicated -> {0} ", this.numOfClients);
+                        break;
+                    default:
+                        //todo 8: which part of the protocol is implemented here?
+                        replyMsg = Message.confirmed;
+                        Console.ForegroundColor = ConsoleColor.DarkGreen;
+                        Console.WriteLine("[Server] received from the client -> {0} ", msg);
+                        Console.ResetColor();
+                        break;
+                }
+            }
+            catch (Exception e) { Console.Out.WriteLine("[Server] Process Message {0}", e.Message); }
+
+            return replyMsg;
+        }
+    }
+}
